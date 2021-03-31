89fda6a53c36a22252fb313c35cc46d6d0dbad31
raymondsaragih <56239301+raymondsaragih@users.noreply.github.com>
2021-03-31 13:21:44 +0700

    Add files via upload
    desain table

--- index.php
+++ index.php
@@ -8,11 +8,18 @@ $result = mysqli_query($mysqli, "SELECT * FROM bukiu ORDER BY id_buku DESC ");
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<title>Homepage</title>
 </head>
 <body>
+<div class="container">
 <a href="form_buku.php">Add New Book</a><br><br>
-<table width="80%" border="1">
+<table class="table table-bordered">
+    <thead class="thead-dark">
 <tr>
 	<th>ID Buku</th>
 	<th>Kode Buku</th>
@@ -21,8 +28,10 @@ $result = mysqli_query($mysqli, "SELECT * FROM bukiu ORDER BY id_buku DESC ");
 	<th>Penerbit Buku</th>
 	<th>Tahun Penerbit</th>
 	<th>Stok</th>
-	<th>Opsi</th>
+	<th>Fungsi</th>
+	<th></th>
 </tr>
+	</thead>
 <?php
 while($user_data = mysqli_fetch_array($result))
 {
@@ -34,7 +43,8 @@ while($user_data = mysqli_fetch_array($result))
 	echo "<td>".$user_data['penerbit_buku']."</td>";
 	echo "<td>".$user_data['tahun_penerbit']."</td>";
 	echo "<td>".$user_data['stok']."</td>";
-	echo "<td><a href='edit.php?id=$user_data[id_buku]'>Edit</a> | <a href='delete.php?id=$user_data[id_buku]'>Delete</a></td></tr>";  
+	echo "<td><a href='edit.php?id=$user_data[id_buku]'>Edit</a>";
+	echo "<td><a href='delete.php?id=$user_data[id_buku]'>Delete</a></td></tr>";
 }
 ?>
 </table>
