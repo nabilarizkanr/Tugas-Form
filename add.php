66a86918e5f183ede1db9fa2b04dbecc780da837
Nabila Rizka <56375525+nabilarizkanr@users.noreply.github.com>
2021-04-01 17:48:00 +0700

    Merubah isi add.php

--- add.php
+++ add.php
@@ -1,20 +1,31 @@
 <?php
-//Kalau Form submitted, insert data ke table bukiu
-if(isset($_POST['Submit'])) {
-    $kode_buku = $_POST['kode_buku'];
-    $judul_buku = $_POST['judul_buku'];
-    $penulis_buku = $_POST['penulis_buku'];
-    $penerbit_buku = $_POST['penerbit_buku'];
-    $tahun_penerbit = $_POST['tahun_penerbit'];
-    $stok= $_POST['stok'];
-
     //Include db file koneksi
     include_once("config.php");
 
-    //Insert Data ke Table
-    $result = mysqli_query($mysqli,"INSERT INTO bukiu(kode_buku,judul_buku,penulis_buku,penerbit_buku,tahun_penerbit,stok) VALUES ('$kode_buku','judul_buku','penulis_buku','penerbit_buku','tahun_penerbit','stok')");
-
-    //Show Message kalau sukses...
-    echo "Added Succesfully. <a href='index.php'>View Data</a>";
-}
+    //Jika Tombol simpan diklik
+    if(isset($_POST['Submit']))
+    {
+        $simpan = mysqli_query($koneksi, "INSERT INTO bukiu (kode_buku, judul_buku, penulis_buku, penerbit_buku, tahun_penerbit, stok)
+            VALUES  ('$_POST[kode_buku]',
+                    '$_POST[judul_buku]',
+                    '$_POST[penulis_buku]',
+                    '$_POST[penerbit_buku]',
+                    '$_POST[tahun_penerbit]',
+                    '$_POST[stok]')
+                    ");
+        if($simpan)
+        {
+            echo "<script>
+                    alert('Sukses Menyimpan Data!');
+                    document.location = 'index.php';
+                    </script>";
+        }
+        else
+        {
+            echo "<script>
+                    alert('Gagal Menyimpan Data!');
+                    document.location = 'index.php';
+                    </script>";
+        }
+    }
 ?>
