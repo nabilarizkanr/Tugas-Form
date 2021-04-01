fa15a27755837659a1d2c5ae5eda55784ebd0c70
Nabila Rizka <56375525+nabilarizkanr@users.noreply.github.com>
2021-04-01 17:45:44 +0700

    Menambahkan Fungsi Baru

--- index.php
+++ index.php
@@ -23,6 +23,25 @@ $result = mysqli_query($mysqli, "SELECT * FROM bukiu ORDER BY id_buku DESC ");
 	<th>Stok</th>
 	<th>Opsi</th>
 </tr>
+	<?php
+	$no = 1;
+	$tampil = mysqli_query($koneksi, "SELECT * FROM bukiu ORDER BY id_buku");
+	while($data = mysqli_fetch_array($tampil)) :
+?>
+
+<tr>
+	<td><?=$no++;?></td>
+	<td><?=$data['kode_buku']?></td>
+	<td><?=$data['judul_buku']?></td>
+	<td><?=$data['penulis_buku']?></td>
+	<td><?=$data['penerbit_buku']?></td>
+	<td><?=$data['tahun_penerbit']?></td>
+	<td><?=$data['stok']?></td>
+	<td><a href='edit.php?id=$user_data[id_buku]'>Edit</a> | <a href='delete.php?id=$user_data[id_buku]'>Delete</a></td>
+</tr>
+
+<?php endwhile; // penutup perulangan 
+?> 
 
 </table>
 </body>
