c51eda3099e146e73df483b1b1fdd712c1ba5828
raymondsaragih <56239301+raymondsaragih@users.noreply.github.com>
2021-04-02 23:12:11 +0700

    new tampilan

--- index.php
+++ index.php
@@ -1,8 +1,96 @@
 <?php
 include_once("config.php");
 
-//all users data from db 
-$result = mysqli_query($mysqli, "SELECT * FROM bukiu ORDER BY id_buku DESC "); 
+    //Jika Tombol simpan diklik
+    if(isset($_POST['Submit']))
+    {
+        //Pengujian apakah data diedit atau simpan baru
+        if($_GET['hal'] == "edit")
+        {
+            //Data akan di edit
+            $edit = mysqli_query($koneksi, "UPDATE bukiu SET 
+            	  id_buku		='$_POST[id_buku]', 
+                  kode_buku     ='$_POST[kode_buku]',
+                  judul_buku    ='$_POST[judul_buku]',
+                  penulis_buku  ='$_POST[penulis_buku]',
+                  penerbit_buku ='$_POST[penerbit_buku]',
+                  tahun_penerbit='$_POST[tahun_penerbit]',
+                  stok          ='$_POST[stok]' WHERE id_buku = '$_GET[id_buku]'
+                    ");
+        if($edit)
+        {
+            echo "<script>
+                    alert('Sukses Update Data!');
+                    document.location = 'index.php';
+                    </script>";
+        }
+        else
+        {
+            echo "<script>
+                    alert('Gagal Update Data!');
+                    document.location = 'index.php';
+                    </script>";
+        }
+        }else
+        {
+            //Data akan disimpan baru
+            $simpan = mysqli_query($koneksi, "INSERT INTO bukiu (kode_buku, judul_buku, penulis_buku, penerbit_buku, tahun_penerbit, stok)
+            VALUES  ('$_POST[kode_buku]',
+                    '$_POST[judul_buku]',
+                    '$_POST[penulis_buku]',
+                    '$_POST[penerbit_buku]',
+                    '$_POST[tahun_penerbit]',
+                    '$_POST[stok]')
+                    ");
+            if($simpan)
+            {
+                echo "<script>
+                        alert('Sukses Menyimpan Data!');
+                        document.location = 'index.php';
+                        </script>";
+            }
+            else
+            {
+                echo "<script>
+                        alert('Gagal Menyimpan Data!');
+                        document.location = 'index.php';
+                        </script>";
+            }
+            }
+            
+    }
+
+	//Kalau tekan tombol edit, maka :
+    if(isset($_GET['hal']))
+    {
+    	if($_GET['hal'] == "edit")
+    	{
+    		//Menampilkan data yang diedit
+    		$tampil = mysqli_query($koneksi, "SELECT * FROM bukiu WHERE id_buku = '$_GET[id_buku]'
+    			");
+    		$data = mysqli_fetch_array($tampil);
+    		if($data)
+    		{
+    			$ekode_buku		= $data['kode_buku'];
+    			$ejudul_buku	= $data['judul_buku'];
+    			$epenulis_buku	= $data['penulis_buku'];
+    			$epenerbit_buku	= $data['penerbit_buku'];
+    			$etahun_penerbit= $data['tahun_penerbit'];
+    			$estok			= $data['stok'];
+    		}
+    	}else if($_GET['hal'] == "hapus")
+    	{
+    		$hapus = mysqli_query($koneksi, "DELETE FROM bukiu WHERE id_buku='$_GET[id_buku]' ");
+    		if($hapus)
+    		{
+               echo "<script>
+                    alert('Sukses Hapus Data!');
+                    document.location = 'index.php';
+                    </script>";
+       		 }
+    	}
+
+    	}
 ?>
 
 <!DOCTYPE html>
@@ -16,14 +104,55 @@ $result = mysqli_query($mysqli, "SELECT * FROM bukiu ORDER BY id_buku DESC ");
 	<title>Homepage</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-dark">
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a href="form_buku.php" class="btn btn-light" role="button">Add New Book</a>
-    </li>
-  </ul>
-</nav>
-<br>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1>SIMPLE LIBRARY</h1>
+    <p>Buku Buku Terbaik Di Seluruh Dunia</p>
+  </div>
+</div>
+<div class="container-md mt-3 border bg-white">
+<form action="index.php" method="POST" name="form-input-data">
+   <center><font style="font-family:cursive; font-size:160%;">Form Input Data Buku</font></center>
+   <br><br>
+   <form> 
+   <div class="form-group">
+    <label>Kode Buku</label>
+    <input type="text" class="form-control" name="kode_buku" value="<?=@$ekode_buku?>" maxlength="6" placeholder="Masukkan kode buku">
+  </div>
+  <div class="form-group">
+    <label>Judul Buku</label>
+    <input type="text" class="form-control" name="judul_buku" value="<?=@$ejudul_buku?>" maxlength="30" placeholder="Masukkan judul buku">
+  </div>
+  <div class="form-group">
+    <label>Penulis Buku</label>
+    <input type="text" class="form-control" name="penulis_buku" value="<?=@$epenulis_buku?>" maxlength="30" placeholder="Masukkan penulis buku">
+  </div>
+  <div class="form-group">
+    <label>Penerbit Buku</label>
+    <input type="text" class="form-control" name="penerbit_buku" value="<?=@$epenerbit_buku?>" maxlength="30"  placeholder="Masukkan penerbit buku">
+  </div>
+  <div class="form-group">
+    <label>Tahun Terbit</label>
+    <input type="text" maxlength="8" class="form-control" name="tahun_penerbit" value="<?=@$etahun_penerbit?>" placeholder="Masukkan tahun terbit buku">
+  </div>
+  <div class="form-group">
+    <label>Stok</label>
+    <input type="text" class="form-control" name="stok" value="<?=@$estok?>" maxlength="12"  placeholder="Masukkan stok buku">
+  </div>
+  <div class="form-group">
+    <label>Penerbit Buku</label>
+    <input type="text" class="form-control" placeholder="Masukkan penerbit buku">
+  </div>
+  <br><center>
+  <input type="submit" name="Submit" value="submit" class="btn btn-info">   
+                <input type="reset" name="reset" value="Cancel" class="btn btn-danger"></td>
+        </tr>
+</form><br>
+</div>
+<div class="container-md mt-3 border bg-white">
+<form action="index.php" method="POST" name="form-input-data">
+   <center><font style="font-family:cursive; font-size:160%;">Data Buku</font></center>
+   <br>
 <div class="container-sm mt-3 ">
 <table class="table table-bordered">
     <thead class="thead-dark">
@@ -38,22 +167,26 @@ $result = mysqli_query($mysqli, "SELECT * FROM bukiu ORDER BY id_buku DESC ");
 	<th colspan="2"><center>Fungsi</center></th>
 </tr>
 	</thead>
-<?php
-while($user_data = mysqli_fetch_array($result))
-{
-	echo "<tr>";
-	echo "<td>".$user_data['id_buku']."</td>";
-	echo "<td>".$user_data['kode_buku']."</td>";
-	echo "<td>".$user_data['judul_buku']."</td>";
-	echo "<td>".$user_data['penulis_buku']."</td>";
-	echo "<td>".$user_data['penerbit_buku']."</td>";
-	echo "<td>".$user_data['tahun_penerbit']."</td>";
-	echo "<td>".$user_data['stok']."</td>";
-	echo "<td><a class='btn btn-info' href='edit.php?id=$user_data[id_buku]'>Edit</a>";
-	echo "<td><a class='btn btn-danger' href='delete.php?id=$user_data[id_buku]'>Delete</a></td></tr>";
-}
-?>
+		<?php
+			
+			$tampil = mysqli_query($koneksi, "SELECT * FROM bukiu ORDER BY id_buku");
+			while($data = mysqli_fetch_array($tampil)) :
+		?>
+
+		<tr>
+			<td><?=$data['id_buku'];?></td>
+			<td><?=$data['kode_buku']?></td>
+			<td><?=$data['judul_buku']?></td>
+			<td><?=$data['penulis_buku']?></td>
+			<td><?=$data['penerbit_buku']?></td>
+			<td><?=$data['tahun_penerbit']?></td>
+			<td><?=$data['stok']?></td>
+			<td><a  class='btn btn-info' href='index.php?hal=edit&id_buku=<?=$data['id_buku']?>'>Edit</a> | <a  class='btn btn-danger' href='index.php?hal=hapus&id<?=$data['id_buku']?>' onclick="return confirm('Apakah yakin ingin menghapus data ini?')">Delete</a></td>
+		</tr>
+
+		<?php endwhile; // penutup perulangan 
+		?> 
+
 </table>
-</div>
 </body>
 </html>
