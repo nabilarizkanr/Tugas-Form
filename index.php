5c51fb0d2e01c8eee0508ce306d30726d7cde874
Nabila Rizka <56375525+nabilarizkanr@users.noreply.github.com>
2021-04-03 22:57:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -97,15 +97,89 @@ if(isset($_POST['submit']))
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	<title>Homepage</title>
+    <title>Homepage</title>
 </head>
 <body>
-
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1>SIMPLE LIBRARY</h1>
+    <p>Buku Buku Terbaik Di Seluruh Dunia</p>
+  </div>
+</div>
+<div class="container-md mt-3 border bg-white">
+<form action="index.php" method="POST" name="form-input-data">
+   <center><font style="font-family:cursive; font-size:160%;">Form Input Data Buku</font></center>
+   <br><br>
+   <form> 
+   <div class="form-group">
+    <label>Kode Buku</label>
+    <input type="text" class="form-control" name="kode_buku" value="<?=@$ekode_buku?>" maxlength="6" placeholder="Masukkan kode buku">
+  </div>
+  <div class="form-group">
+    <label>Judul Buku</label>
+    <input type="text" class="form-control" name="judul_buku" value="<?=@$ejudul_buku?>" maxlength="30" placeholder="Masukkan judul buku">
+  </div>
+  <div class="form-group">
+    <label>Penulis Buku</label>
+    <input type="text" class="form-control" name="penulis_buku" value="<?=@$epenulis_buku?>" maxlength="30" placeholder="Masukkan penulis buku">
+  </div>
+  <div class="form-group">
+    <label>Penerbit Buku</label>
+    <input type="text" class="form-control" name="penerbit_buku" value="<?=@$epenerbit_buku?>" maxlength="30"  placeholder="Masukkan penerbit buku">
+  </div>
+  <div class="form-group">
+    <label>Tahun Terbit</label>
+    <input type="text" maxlength="8" class="form-control" name="tahun_penerbit" value="<?=@$etahun_penerbit?>" placeholder="Masukkan tahun terbit buku">
+  </div>
+  <div class="form-group">
+    <label>Stok</label>
+    <input type="text" class="form-control" name="stok" value="<?=@$estok?>" maxlength="12"  placeholder="Masukkan stok buku">
+  </div>
+  <br><center>
+  <input type="submit" name="Submit" value="submit" class="btn btn-info">   
+                <input type="reset" name="reset" value="Cancel" class="btn btn-danger"></td>
+        </tr>
+</form><br>
+</div>
+<div class="container-md mt-3 border bg-white">
+<form action="index.php" method="POST" name="form-input-data">
+   <center><font style="font-family:cursive; font-size:160%;">Data Buku</font></center>
+   <br>
+<div class="container-sm mt-3 ">
+<table class="table table-bordered">
+    <thead class="thead-dark">
+<tr>
+    <th><center>ID Buku</center></th>
+    <th><center>Kode Buku</center></th>
+    <th><center>Judul Buku</center></th>
+    <th><center>Penulis Buku</center></th>
+    <th><center>Penerbit Buku</center></th>
+    <th><center>Tahun Penerbit</center></th>
+    <th><center>Stok</center></th>
+    <th colspan="2"><center>Fungsi</center></th>
+</tr>
+    </thead>
         <?php
             $no = 1;
             $tampil = mysqli_query($koneksi, "SELECT * FROM bukiu ORDER BY id_buku");
             while($data = mysqli_fetch_array($tampil)):
         ?>
+
+        <tr>
+            <td><?=$no++;?></td>
+            <td><?=$data['kode_buku']?></td>
+            <td><?=$data['judul_buku']?></td>
+            <td><?=$data['penulis_buku']?></td>
+            <td><?=$data['penerbit_buku']?></td>
+            <td><?=$data['tahun_penerbit']?></td>
+            <td><?=$data['stok']?></td>
+            <td><a class='btn btn-info' href='index.php?hal=edit&id_buku=<?=$data['id_buku']?>'>Edit</a> | 
+                <a class='btn btn-danger' href='index.php?hal=hapus&id_buku=<?=$data['id_buku']?>' 
+                onclick="return confirm('Apakah yakin ingin menghapus data ini?')">Delete</a></td>
+        </tr>
+
+        <?php endwhile; // penutup perulangan 
+        ?> 
 
 </table>
 </body>
